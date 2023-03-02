a6ee957b0f11d6bfe157965db694a89d4fc98259
milkavladislav <milkav06062003@gmail.com>
2023-03-02 11:41:10 +0200

    add createUser and createProperty methods with cloudinary media service

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,23 @@ import User from "../mongodb/models/user.js";
 
 const getAllUsers = async (req, res) => {};
 
-const createUser = async (req, res) => {};
+const createUser = async (req, res) => {
+  try {
+    const { name, email, avatar } = new User(req.body);
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(200).json(userExists);
+    }
+
+    const newUser = await User.create({ name, email, avatar });
+
+    return res.status(201).json(newUser);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 const getUserById = async (req, res) => {};
 
