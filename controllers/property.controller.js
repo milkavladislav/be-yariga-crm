ce934383cf71b6517187358a0225a3753222b31e
milkavladislav <milkav06062003@gmail.com>
2023-03-05 09:09:11 +0200

    add controller for get all properties

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -13,7 +13,14 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const getAllProperties = async (req, res) => {};
+const getAllProperties = async (req, res) => {
+  try {
+    const properties = await Property.find({}).limit(req.query._end);
+    res.status(200).json(properties);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 const getPropertyDetail = async (req, res) => {};
 
